7d78fd344c0c2f75d85d3e9db7ee3c65dd0474c6
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-08-18 19:43:20 +0700

    Reduce boiler plate

--- Bubble.js
+++ Bubble.js
@@ -3,22 +3,18 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 function bubbleSort(array) {
 
     for (let i = array.length - 1; i >= 0; i--) {
-        let checkerA = 0
-        let checkerB = 1
-        while (checkerB <= i) {
-            if (array[checkerA] > array[checkerB]) {
-                const valueA = array[checkerA]
-                const valueB = array[checkerB]
-                array[checkerA] = valueB
-                array[checkerB] = valueA
+        let checker = 0
+        while (checker + 1 <= i) {
+            if (array[checker] > array[checker + 1]) {
+                const valueHolder = array[checker]
+                array[checker] = array[checker + 1]
+                array[checker + 1] = valueHolder
             }
-            checkerA++
-            checkerB++
+            checker++
         }
     }
 
     return array
 }
 
-console.log(numbers)
 console.log(bubbleSort(numbers))
