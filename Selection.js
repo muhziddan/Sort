6b187de363891e138f24ba54fcc53a74e5a75280
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-08-18 20:38:19 +0700

    finished the selection sort algorithm

--- Selection.js
+++ Selection.js
@@ -1,7 +1,20 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
-  
+    for (let i = 0; i < array.length - 1; i++) {
+        let position = 0
+        let minimumValue = array[i]
+        for (let j = i + 1; j < array.length; j++) {
+            if (minimumValue > array[j]) {
+                minimumValue = array[j]
+                position = j
+            }
+        }
+        array[position] = array[i]
+        array[i] = minimumValue
+    }
+
+    return array
 }
 
-selectionSort(numbers);
+console.log(selectionSort(numbers))
