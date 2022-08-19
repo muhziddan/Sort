d8d185903f7e1d11f7ce5678d32566ab9e42876f
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-08-19 09:07:50 +0700

    add same value handler

--- Insertion.js
+++ Insertion.js
@@ -1,13 +1,20 @@
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+const numbers = [99, 44, 6, 2, 1, 44, 63, 283, 283, 2, 0]
+// const numbers = [99, 44, 6, 6, 5, 5]
 
 function insertionSort(array) {
 
     for (let i = 0; i < array.length; i++) {
-        if (array[i] < array[0]) {
+        if (array[i] <= array[0]) {
+            // check the current index with the first value of the array
+            // if the current value is lower than the first index, it will be spliced then unshift the spliced value
             array.unshift(array.splice(i,1)[0])
         } else {
+            console.log('called bottom')
             for (let j = 1; j < i; j++) {
+                // why j = 1? because if the array/check stage only consist of 2 value, it will be handled from previous if statement
                 if (array[i] > array[j-1] && array[i] < array[j]) {
+                    array.splice(j,0,array.splice(i,1)[0])
+                } else if (array[i] === array[j]) {
                     array.splice(j,0,array.splice(i,1)[0])
                 }
             }
