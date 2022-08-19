21944b5947530322d176355561d7a692c8cd5f6d
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-08-19 18:38:37 +0700

    add sorting question and answer file

---
+++ SortingQuestion.js
@@ -0,0 +1,23 @@
+//#1 - Sort 10 schools around your house by distance:
+// Insertion Sort
+
+//#2 - eBay sorts listings by the current Bid amount:
+// Radix or counting
+
+//#3 - Sport scores on ESPN
+// Quick Sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// Heap/Quick Sort -> Quick
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// Insertion Sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+// Quick Sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+// Merge Sort
+
+//#8 - You want to teach sorting for the first time
+// Bubble, Selection
